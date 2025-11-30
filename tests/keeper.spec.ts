0d51a733a474c621280abdb80202839075d91532
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2025-11-29 16:38:45 -0800

    wrote 4 initial tests
    a lot of functionality doesn't work while I'm writing tests
    In general now login is broken so not many tests I can automate

--- tests/keeper.spec.ts
+++ tests/keeper.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only("register new user", async ({ page }) =>
+test("register new user", async ({ page }) =>
 {
     await page.goto('https://parabank.parasoft.com/parabank/index.html', { waitUntil: 'domcontentloaded' });
 
@@ -29,3 +29,69 @@ test.only("register new user", async ({ page }) =>
     await expect(page.getByRole('heading', { name: `Welcome ${username}` })).toBeVisible();
     //Welcome john_doe_test_001
 });
+
+test('Login', async ({ page }) =>
+{
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm', { waitUntil: 'domcontentloaded' });
+    //await page.waitForURL('https://parabank.parasoft.com/parabank/login.htm;jsessionid=**');
+    let username = 'john_doe_test_100101';
+    let password = 'P@$sw0rd_123!';
+    await page.locator('input[name="username"]').fill(username);//#customer.username
+    await page.locator('input[name="password"]').fill(password);// P@$sw0rd_123!
+
+    await page.getByRole("button", { name: 'Log In' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Error' })).toBeVisible(); // Error is happening while I'm writing this test
+});
+
+test('Forgot info', async ({ page }) => 
+{
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm', { waitUntil: 'domcontentloaded' });
+
+    await page.getByRole('link', { name: 'Forgot login info?' }).click();
+    let item = 'test';
+    await page.locator('input[name="firstName"]').fill(item);
+    await page.locator('input[name="lastName"]').fill(item);
+    await page.locator('input[name="firstName"]').fill(item);
+    await page.locator('input[name="address.street"]').fill(item);
+    await page.locator('input[name="address.city"]').fill(item);//#customer.address.city
+    await page.locator('input[name="address.state"]').fill(item);// #customer.address.state
+    await page.locator('input[name="address.zipCode"]').fill(item);// #customer.address.zipCode
+    await page.locator('input[name="ssn"]').fill(item);
+
+    await page.getByRole('button', { name: 'Find My Login Info' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Customer Lookup' })).toBeVisible();
+});
+
+test.only('Request a loan', async ({ page }) =>
+{
+
+    //PLEASE READ: a lot of functionality doesn't work while I'm writing tests
+    //In general now login is broken but I found a hack how to login  - bu requesting an info
+    //-------------------- prerequisits --------------------
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm', { waitUntil: 'domcontentloaded' });
+
+    await page.getByRole('link', { name: 'Forgot login info?' }).click();
+    let item = 'test';
+    await page.locator('input[name="firstName"]').fill(item);
+    await page.locator('input[name="lastName"]').fill(item);
+    await page.locator('input[name="firstName"]').fill(item);
+    await page.locator('input[name="address.street"]').fill(item);
+    await page.locator('input[name="address.city"]').fill(item);//#customer.address.city
+    await page.locator('input[name="address.state"]').fill(item);// #customer.address.state
+    await page.locator('input[name="address.zipCode"]').fill(item);// #customer.address.zipCode
+    await page.locator('input[name="ssn"]').fill(item);
+
+    await page.getByRole('button', { name: 'Find My Login Info' }).click();
+
+    await expect(page.getByRole('heading', { name: 'Customer Lookup' })).toBeVisible();
+    // ====================== Actual test ===============
+    await page.getByRole('link', { name: 'Request Loan' }).click();
+
+    await page.locator('#amount').fill('1000');
+    await page.locator('#downPayment').fill('100');
+
+    await page.getByRole('button', { name: 'Apply Now' }).click();
+    await expect(page.getByRole('heading', { name: 'Error' })).toBeVisible(); // Error is happening while I'm writing this test
+})
